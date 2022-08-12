8933148bcffdb8fa7e75c6da5fcdb34a41440c2b
Dzhanet <104907844+Dzhanet@users.noreply.github.com>
2022-08-13 00:59:33 +0200

    Delete hw_17 directory

--- hw_17/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'test.db') #абсолютный путь относительно файла config до БД
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- hw_17/run.py
+++
@@ -1,6 +0,0 @@
-from application.app import create_app
-
-#импортируем функцию, объявляющую фласк и запускаем приложение
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
